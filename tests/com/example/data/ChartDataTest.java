65c07b73ff017cae1a4e5c6dad75b831e8b04721
Wayne Volkmuth <volkmuth@gmail.com>
2011-01-11 18:31:17 -0800

    Clean up unused imports

--- tests/com/example/data/ChartDataTest.java
+++ tests/com/example/data/ChartDataTest.java
@@ -2,10 +2,7 @@ package com.example.data;
 
 import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ChartDataTest {
